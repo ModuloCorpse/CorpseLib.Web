af84493e0280fad585785444ea3885295e7c85ff
ModuloCorpse <mewen.lereste@wanadoo.fr>
2024-09-05 18:20:15 +0200

    [EDIT] OAuth2 Authenticator improvements
    - OAuth2 Authenticator now store public and private keys and scope
    - RefreshToken can now be stored in a secured file and retrieve from there

--- OAuth/Authenticator.cs
+++ OAuth/Authenticator.cs
@@ -1,10 +1,12 @@
-﻿using CorpseLib.Network;
+﻿using CorpseLib.Encryption;
+using CorpseLib.Network;
 using CorpseLib.Web.Http;
 using System.Diagnostics;
+using System.Runtime.Versioning;
 
 namespace CorpseLib.Web.OAuth
 {
-    public class Authenticator
+    public class Authenticator(string[] scopes, string @public, string @private, string host, string authorizePath, string tokenPath, string redirectPath, int redirectPort)
     {
         private class AuthenticatorClientProtocol(Operation<RefreshToken> tokenOperation, URI redirectURL, string[] expectedScope, string publicKey, string privateKey, string pageContent, string host, string expectedState, string tokenPath) : HttpProtocol
         {
@@ -54,68 +56,40 @@ namespace CorpseLib.Web.OAuth
             }
         }
 
+        [SupportedOSPlatform("windows")]
+        private readonly WindowsEncryptionAlgorithm m_WindowsEncryptionAlgorithm = new([95, 239, 5, 252, 160, 29, 242, 88, 31, 3]);
         private Operation<RefreshToken> m_TokenOperation = new();
         private string m_PageContent = string.Empty;
-        private readonly string m_Host;
-        private readonly string m_AuthorizePath = "/oauth2/authorize";
-        private readonly string m_TokenPath = "/oauth2/token";
-        private readonly string m_RedirectPath = "/oauth_authenticate";
-        private readonly int m_RedirectPort = 80;
+        private readonly string[] m_Scopes = scopes;
+        private readonly string m_PublicKey = @public;
+        private readonly string m_PrivateKey = @private;
+        private readonly string m_Host = host;
+        private readonly string m_AuthorizePath = authorizePath;
+        private readonly string m_TokenPath = tokenPath;
+        private readonly string m_RedirectPath = redirectPath;
+        private readonly int m_RedirectPort = redirectPort;
 
-        public Authenticator(string host) => m_Host = host;
-
-        public Authenticator(string host, string redirectPath)
-        {
-            m_Host = host;
-            m_RedirectPath = redirectPath;
-        }
-
-        public Authenticator(string host, string redirectPath, int redirectPort)
-        {
-            m_Host = host;
-            m_RedirectPath = redirectPath;
-            m_RedirectPort = redirectPort;
-        }
-
-        public Authenticator(string host, string authorizePath, string tokenPath)
-        {
-            m_Host = host;
-            m_AuthorizePath = authorizePath;
-            m_TokenPath = tokenPath;
-        }
-
-        public Authenticator(string host, string authorizePath, string tokenPath, string redirectPath)
-        {
-            m_Host = host;
-            m_AuthorizePath = authorizePath;
-            m_TokenPath = tokenPath;
-            m_RedirectPath = redirectPath;
-        }
-
-        public Authenticator(string host, string authorizePath, string tokenPath, string redirectPath, int redirectPort)
-        {
-            m_Host = host;
-            m_AuthorizePath = authorizePath;
-            m_TokenPath = tokenPath;
-            m_RedirectPath = redirectPath;
-            m_RedirectPort = redirectPort;
-        }
+        public Authenticator(string[] scopes, string publicKey, string privateKey, string host) : this(scopes, publicKey, privateKey, host, "/oauth2/authorize", "/oauth2/token", "/oauth_authenticate", 80) { }
+        public Authenticator(string[] scopes, string publicKey, string privateKey, string host, string redirectPath) : this(scopes, publicKey, privateKey, host, "/oauth2/authorize", "/oauth2/token", redirectPath, 80) { }
+        public Authenticator(string[] scopes, string publicKey, string privateKey, string host, string redirectPath, int redirectPort) : this(scopes, publicKey, privateKey, host, "/oauth2/authorize", "/oauth2/token", redirectPath, redirectPort) { }
+        public Authenticator(string[] scopes, string publicKey, string privateKey, string host, string authorizePath, string tokenPath) : this(scopes, publicKey, privateKey, host, authorizePath, tokenPath, "/oauth_authenticate", 80) { }
+        public Authenticator(string[] scopes, string publicKey, string privateKey, string host, string authorizePath, string tokenPath, string redirectPath) : this(scopes, publicKey, privateKey, host, authorizePath, tokenPath, redirectPath, 80) {}
 
         public void SetPageContent(string content) => m_PageContent = content;
 
-        public OperationResult<RefreshToken> AuthorizationCode(string[] expectedScope, string publicKey, string privateKey, string browser = "")
+        public OperationResult<RefreshToken> AuthorizationCode(string browser = "")
         {
             m_TokenOperation = new();
             URI redirectURL = URI.Build("http").Host("localhost").Port(m_RedirectPort).Path(m_RedirectPath).Build();
             string expectedState = Guid.NewGuid().ToString();
-            string scopeString = string.Join('+', expectedScope).Replace(":", "%3A");
+            string scopeString = string.Join('+', m_Scopes).Replace(":", "%3A");
             URI oauthURL = URI.Build("https")
                 .Host(m_Host)
                 .Path(m_AuthorizePath)
                 .Query(new URIQuery('&')
                 {
                     { "response_type", "code" },
-                    { "client_id", publicKey },
+                    { "client_id", m_PublicKey },
                     { "redirect_uri", redirectURL.ToString() },
                     { "scope", scopeString },
                     { "state", expectedState }
@@ -131,11 +105,45 @@ namespace CorpseLib.Web.OAuth
                 myProcess.StartInfo.Arguments = oauthURL.ToString();
             }
             myProcess.Start();
-            TCPAsyncServer httpServer = new(() => new AuthenticatorClientProtocol(m_TokenOperation, redirectURL, expectedScope, publicKey, privateKey, m_PageContent, m_Host, expectedState, m_TokenPath), redirectURL.Port);
+            TCPAsyncServer httpServer = new(() => new AuthenticatorClientProtocol(m_TokenOperation, redirectURL, m_Scopes, m_PublicKey, m_PrivateKey, m_PageContent, m_Host, expectedState, m_TokenPath), redirectURL.Port);
             httpServer.Start();
             m_TokenOperation.Wait();
             httpServer.Stop();
             return m_TokenOperation.Result;
         }
+
+        public void SaveToken(string path, RefreshToken token)
+        {
+            string content = string.Format("{0}\n{1}", token.AccessToken, token.TokenRefresh);
+            if (OperatingSystem.IsWindows())
+            {
+                EncryptedFile encryptedFile = new(path) { m_WindowsEncryptionAlgorithm };
+                encryptedFile.Write(content);
+            }
+            else
+                File.WriteAllText(path, content);
+        }
+
+        public RefreshToken? LoadToken(string path)
+        {
+            string content;
+            if (OperatingSystem.IsWindows())
+            {
+                EncryptedFile encryptedFile = new(path) { m_WindowsEncryptionAlgorithm };
+                content = encryptedFile.Read();
+            }
+            else
+                content = File.ReadAllText(path);
+            string[] lines = content.Split('\n');
+            if (lines.Length == 2)
+            {
+                RefreshToken ret = new(m_Scopes, m_PrivateKey, URI.Build("https").Host(m_Host).Port(443).Path(m_TokenPath).Build(), lines[1], m_PublicKey, lines[0]);
+                if (ret.Refresh())
+                    return ret;
+            }
+            return null;
+        }
+
+        public RefreshToken CreateToken(string access, string refresh) => new(m_Scopes, m_PrivateKey, URI.Build("https").Host(m_Host).Port(443).Path(m_TokenPath).Build(), refresh, m_PublicKey, access);
     }
 }
--- OAuth/RefreshToken.cs
+++ OAuth/RefreshToken.cs
@@ -16,8 +16,20 @@ namespace CorpseLib.Web.OAuth
 
         public event RefreshEventHandler? Refreshed;
 
-        //TODO Make it internal
-        public RefreshToken(URI url, string[] scopes, string publicKey, string secret, string token, string redirectURI) : base(publicKey)
+        internal string[] Scopes => m_Scopes;
+        internal string Secret => m_Secret;
+        internal URI OAuthURL => m_OAuthURL;
+        internal string TokenRefresh => m_RefreshToken;
+
+        internal RefreshToken(string[] scopes, string secret, URI oauthURL, string refreshToken, string clientID, string accessToken) : base(clientID, accessToken)
+        {
+            m_Scopes = scopes;
+            m_Secret = secret;
+            m_OAuthURL = oauthURL;
+            m_RefreshToken = refreshToken;
+        }
+
+        internal RefreshToken(URI url, string[] scopes, string publicKey, string secret, string token, string redirectURI) : base(publicKey)
         {
             m_OAuthURL = url;
             m_Scopes = scopes;
@@ -25,30 +37,36 @@ namespace CorpseLib.Web.OAuth
             GetAccessToken(string.Format("client_id={0}&client_secret={1}&code={2}&grant_type=authorization_code&redirect_uri={3}", ClientID, m_Secret, token, redirectURI));
         }
 
-        private void GetAccessToken(string request)
+        private bool GetAccessToken(string request)
         {
             URLRequest oauthRequest = new(m_OAuthURL, Request.MethodType.POST, request);
             oauthRequest.AddContentType(MIME.APPLICATION.X_WWW_FORM_URLENCODED);
             Response oauthResponse = oauthRequest.Send();
             string responseJsonStr = oauthResponse.Body;
             if (string.IsNullOrWhiteSpace(responseJsonStr))
-                return;
+                return false;
             DataObject responseJson = JsonParser.Parse(responseJsonStr);
             List<string> scope = responseJson.GetList<string>("scope");
             if (responseJson.TryGet("access_token", out string? access_token) &&
                 responseJson.TryGet("refresh_token", out string? refresh_token) &&
                 responseJson.TryGet("token_type", out string? token_type) && token_type! == "bearer" &&
-                m_Scopes.All(item => scope.Contains(item)) && scope.All(item => m_Scopes.Contains(item)))
+                m_Scopes.All(scope.Contains) && scope.All(m_Scopes.Contains))
             {
                 m_RefreshToken = refresh_token!;
                 SetAccessToken(access_token!);
+                return true;
             }
+            return false;
         }
 
-        public void Refresh()
+        public bool Refresh()
         {
-            GetAccessToken(string.Format("grant_type=refresh_token&refresh_token={0}&client_id={1}&client_secret={2}", m_RefreshToken, ClientID, m_Secret));
-            Refreshed?.Invoke(this);
+            if (GetAccessToken(string.Format("grant_type=refresh_token&refresh_token={0}&client_id={1}&client_secret={2}", m_RefreshToken, ClientID, m_Secret)))
+            {
+                Refreshed?.Invoke(this);
+                return true;
+            }
+            return false;
         }
     }
 }
